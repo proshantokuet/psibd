da8864c101480b7458859323d38554a5a51ecf25
tanvirmpower <tanvir@mpower-social.com>
2019-08-18 19:32:52 +0600

    Add Child server side portion created

---
+++ api/src/main/java/org/openmrs/module/PSI/api/impl/PSIClinicChildServiceImpl.java
@@ -0,0 +1,46 @@
+package org.openmrs.module.PSI.api.impl;
+
+import java.util.List;
+
+import org.openmrs.api.impl.BaseOpenmrsService;
+import org.openmrs.module.PSI.PSIClinicChild;
+import org.openmrs.module.PSI.api.PSIClinicChildService;
+import org.openmrs.module.PSI.api.db.PSIClinicChildDAO;
+
+public class PSIClinicChildServiceImpl extends BaseOpenmrsService implements PSIClinicChildService {
+	
+	private PSIClinicChildDAO dao;
+	
+	public PSIClinicChildDAO getDao() {
+		return dao;
+	}
+	
+	public void setDao(PSIClinicChildDAO dao) {
+		this.dao = dao;
+	}
+
+	@Override
+	public PSIClinicChild saveOrUpdate(PSIClinicChild psiClinicAddChild) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public PSIClinicChild findById(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<PSIClinicChild> findByMotherUuid(String motherUuid) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void delete(int id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
