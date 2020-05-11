713ac5a719363736b5f208612e4db3fcf5696e02
tanvirmpower <tanvir@mpower-social.com>
2020-05-11 19:14:30 +0600

    discharge and birth pdf api

--- api/src/main/java/org/openmrs/module/PSI/api/PSIUniquePatientService.java
+++ api/src/main/java/org/openmrs/module/PSI/api/PSIUniquePatientService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.openmrs.api.OpenmrsService;
 import org.openmrs.module.PSI.PSIClinicChild;
+import org.openmrs.module.PSI.SHNFormPdfDetails;
 import org.openmrs.module.PSI.SHnPrescriptionMetaData;
 
 public interface PSIUniquePatientService  extends OpenmrsService {
@@ -11,5 +12,10 @@ public interface PSIUniquePatientService  extends OpenmrsService {
 	public Boolean findPatientByUicandMobileNo(String patientUic, String mobileNo);
 	
 	public List<SHnPrescriptionMetaData> getAllPrescriptionMetaData();
+	
+	public List <SHNFormPdfDetails> getDischargeInformationByVisit(String patientUuid, String visitUuid);
+	
+	public List <SHNFormPdfDetails> getbirthInformationByVisit(String patientUuid, String visitUuid);
+
 	
 }
